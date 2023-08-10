a6677ecc924709a403585de26f55aba80ecce1fd
bhushan6 <bhushanwagh6@gmail.com>
2023-08-10 23:45:08 +0530

    updates : example for conditional alias

--- example/vite.config.js
+++ example/vite.config.js
@@ -2,14 +2,22 @@ import { defineConfig } from "vite";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
+export default defineConfig(({ mode }) => {
+  let alias = {};
+
+  if (mode === "development") {
+    alias = {
       "babylon-msdf-text": path.resolve(
         __dirname,
         "../package/MSDF-Text/index.js"
       ),
+    };
+  }
+
+  return {
+    resolve: {
+      alias: alias,
     },
-  },
-  plugins: [],
+    plugins: [],
+  };
 });
